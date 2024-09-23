5f7114791b163f9e410b7b7724be27fb862912ab
Rafael Cathomen <rafael@cathomen.ch>
2024-09-23 20:54:14 +0200

    basic grab action working

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/actions/actions.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/actions/actions.py
@@ -24,13 +24,19 @@ class SimpleAction(ActionTerm):
         self.robot_name = "robot"
         self.env = env
 
-        self.max_velocity = 1.0
-        self.max_angular_velocity = 1.0
+        self.max_force = 2.0  # Newtons
+        self.max_torque = 1.0  # Newton-meters
 
         self._processed_actions = torch.zeros(3)
 
+        # command buffers
         self.force_command = torch.zeros(env.num_envs, 3)
         self.torque_command = torch.zeros(env.num_envs, 3)
+
+        # object pos buffers
+        self.grabbed_asset_pos_body_frame = torch.zeros(env.num_envs, 3).to(env.device)
+        self.grabbed_asset_quat_body_frame = torch.zeros(env.num_envs, 4).to(env.device)
+        self.grabbed_asset_id = torch.zeros(env.num_envs).to(env.device) - 1
 
     """
     Properties.
@@ -39,7 +45,7 @@ class SimpleAction(ActionTerm):
     @property
     def action_dim(self) -> int:
         """Dimension of the action term."""
-        return 3
+        return 3 + 1  # x, y, yaw (continuous), grab (binary)
 
     @property
     def raw_actions(self) -> torch.Tensor:
@@ -64,13 +70,24 @@ class SimpleAction(ActionTerm):
         Args:
             actions: The actions to process.
         """
-        xy_force_body = actions[:, :2] * self.max_velocity
-        yaw_torque = actions[:, 2] * self.max_angular_velocity
+
+        # - movement x, y, yaw:
+        # movement is implemented as a force in the x, y direction and a torque in the yaw direction
+        xy_force_body = actions[:, :2] * self.max_force
+        yaw_torque = actions[:, 2] * self.max_torque
+
+        xy_force_body = torch.clamp(xy_force_body, -self.max_force, self.max_force)
+        yaw_torque = torch.clamp(yaw_torque, -self.max_torque, self.max_torque)
 
         self.force_command = torch.cat([xy_force_body, torch.zeros(self.env.num_envs, 1).to(self.device)], dim=1)
         self.torque_command = torch.cat(
             [torch.zeros(self.env.num_envs, 2).to(self.device), yaw_torque.unsqueeze(1)], dim=1
         )
+
+        # - grab:
+        # when grab is activated (< 0), the closest object within a radius becomes movable for this agent
+        self.is_grabbing = actions[:, 3] > -1000000
+        # TODO facing direction
 
     def apply_actions(self):
         """Applies the actions to the asset managed by the term.
@@ -79,9 +96,63 @@ class SimpleAction(ActionTerm):
             This is called at every simulation step by the manager.
         """
 
+        # - moving action
         # get forces
         # TODO put in process_actions
 
         self._asset.set_external_force_and_torque(
             forces=self.force_command.unsqueeze(1), torques=self.torque_command.unsqueeze(1)
         )
+
+        # - grab actions:
+        min_distance = 4.0  # meters
+
+        # robot quaternion
+        robot_quat = math_utils.yaw_quat(self.env.scene.rigid_objects["robot"].data.root_quat_w)
+
+        # get positions
+        robot_pos = self.env.scene.rigid_objects["robot"].data.root_pos_w
+
+        # TODO: fix this, does not work properly
+
+        asset_ids = ["asset_1", "asset_2", "asset_3"]
+        for id_num, asset_id in enumerate(asset_ids):
+            asset_pos = self.env.scene.rigid_objects[asset_id].data.root_pos_w
+
+            # get distances
+            dist_vec = asset_pos - robot_pos
+            dist = torch.linalg.vector_norm(dist_vec, dim=1)
+            # temporary action disabled
+            grab_asset = (dist < min_distance) & self.is_grabbing
+
+            # the boxes are kinematic bodies, meaning they cannot be pushed.
+            # we move them by setting their position directly
+
+            # if the agent was grabbing previously and its still grabing, we update the position of the grabbed asset.
+            still_grabbing = (self.grabbed_asset_id == id_num) & grab_asset
+            if still_grabbing.any():
+                asset_pos_body_frame = self.grabbed_asset_pos_body_frame[still_grabbing]
+                asset_quat_body_frame = self.grabbed_asset_quat_body_frame[still_grabbing]
+                asset_pos_world_frame = (
+                    math_utils.quat_apply(robot_quat[still_grabbing], asset_pos_body_frame) + robot_pos[still_grabbing]
+                )
+                asset_quat_world_frame = math_utils.quat_mul(robot_quat[still_grabbing], asset_quat_body_frame)
+                asset_quat = self.env.scene.rigid_objects[asset_id].data.root_quat_w
+                # fill pose tensor for all envs to write to sim
+                asset_quat[still_grabbing] = asset_quat_world_frame
+                asset_pos[still_grabbing] = asset_pos_world_frame
+                asset_pose_world_frame = torch.cat([asset_pos, asset_quat], dim=1)
+                self.env.scene.rigid_objects[asset_id].write_root_pose_to_sim(asset_pose_world_frame)
+
+            # if we start grabbing now, we store the relative position of the asset to the robot and set grabbed_asset to True
+            now_grabbing = grab_asset & ~still_grabbing
+            if now_grabbing.any():
+                # set the position of the grabbed asset in the body frame of the robot
+                self.grabbed_asset_pos_body_frame[now_grabbing] = math_utils.quat_apply(
+                    math_utils.quat_inv(robot_quat[now_grabbing]), dist_vec[now_grabbing]
+                )
+                self.grabbed_asset_quat_body_frame[now_grabbing] = math_utils.quat_mul(
+                    math_utils.quat_inv(robot_quat[now_grabbing]),
+                    self.env.scene.rigid_objects[asset_id].data.root_quat_w[now_grabbing],
+                )
+                self.grabbed_asset_id[now_grabbing] = id_num
--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/assets.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/assets.py
@@ -4,16 +4,18 @@ from omni.isaac.lab.assets import ArticulationCfg, AssetBaseCfg, RigidObject, Ri
 # - robot:
 ROBOT_CFG = RigidObjectCfg(
     prim_path="{ENV_REGEX_NS}/Robot",
-    spawn=sim_utils.SphereCfg(
-        radius=0.5,
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(max_depenetration_velocity=1.0, disable_gravity=False),
+    spawn=sim_utils.CuboidCfg(
+        size=(0.8, 1.2, 0.6),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            max_depenetration_velocity=1.0, disable_gravity=False, max_linear_velocity=3.0, max_angular_velocity=1.5
+        ),
         mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
-        physics_material=sim_utils.RigidBodyMaterialCfg(),
+        physics_material=sim_utils.RigidBodyMaterialCfg(static_friction=0.1, dynamic_friction=0.1),
         collision_props=sim_utils.CollisionPropertiesCfg(),
         visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.2, 0.9, 0.2)),
     ),
-    init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, 0.5)),
-    collision_group=-1,
+    init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, 0.0)),
+    collision_group=0,
 )
 
 # - movable objects:
@@ -23,29 +25,32 @@ CUBOID_CFG = RigidObjectCfg(
     prim_path="{ENV_REGEX_NS}/Cuboid",
     spawn=sim_utils.CuboidCfg(
         size=(0.5, 0.5, 0.5),
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(max_depenetration_velocity=1.0, disable_gravity=False),
-        mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
-        physics_material=sim_utils.RigidBodyMaterialCfg(),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            max_depenetration_velocity=1.0, disable_gravity=False, max_angular_velocity=3.14, kinematic_enabled=True
+        ),
+        mass_props=sim_utils.MassPropertiesCfg(mass=0.5),
+        physics_material=sim_utils.RigidBodyMaterialCfg(static_friction=0.5, dynamic_friction=0.5),
         collision_props=sim_utils.CollisionPropertiesCfg(),
         visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.9, 0.2, 0.2)),
     ),
-    init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, 0.5)),
-    collision_group=-1,
+    init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, 0.0)),
+    collision_group=0,
 )
 
 # - wall:
 WALL_CFG = RigidObjectCfg(
     prim_path="{ENV_REGEX_NS}/Wall",
     spawn=sim_utils.CuboidCfg(
-        size=(0.5, 2.5, 0.5),
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(max_depenetration_velocity=1.0, disable_gravity=False),
+        size=(0.5, 3.5, 1.0),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            max_depenetration_velocity=1.0, disable_gravity=False, kinematic_enabled=True
+        ),
         mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
-        physics_material=sim_utils.RigidBodyMaterialCfg(),
         collision_props=sim_utils.CollisionPropertiesCfg(),
-        visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.9, 0.2, 0.2)),
+        visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.7, 0.7, 0.7)),
     ),
-    init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, 0.5)),
-    collision_group=-1,
+    init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, 0.0)),
+    collision_group=0,
 )
 
 # TODO: Add more objects here, ramp
