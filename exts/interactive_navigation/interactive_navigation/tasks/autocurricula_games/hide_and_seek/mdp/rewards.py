23e38ab0b0217f5f33f3870ef72460c2c37d68b2
Rafael Cathomen <rafael@cathomen.ch>
2024-09-24 11:29:14 +0200

    added dummy reward (doesnt do anything yet)

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/rewards.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/rewards.py
@@ -12,5 +12,25 @@ if TYPE_CHECKING:
 
 def dummy_reward(env: ManagerBasedRLEnv, sensor_cfg: SceneEntityCfg, threshold: float) -> torch.Tensor:
     """Placeholder reward function that returns a dummy reward."""
+    asset_ids = ["asset_1", "asset_2", "asset_3"]
+
+    asset_poses = []
+    for asset_id in asset_ids:
+        asset_poses.append(env.scene.rigid_objects[asset_id].data.root_pos_w)
+
+    positions = torch.stack(asset_poses)
+    # positions is expected to be of shape (N, 2)
+    diff = positions.unsqueeze(1) - positions.unsqueeze(0)  # shape (N, N, 2)
+    dist_squared = (diff**2).sum(-1)  # shape (N, N)
+
+    # Convert squared distances to actual distances
+    dist = torch.sqrt(dist_squared)
+
+    # Since dist[i, j] and dist[j, i] are the same, we can sum up one triangle and diagonal, then double it.
+    # We avoid doubling the diagonal since it is always zero.
+    triu_indices = torch.triu_indices(dist.size(0), dist.size(1), offset=1)
+    sum_distances = dist[triu_indices[0], triu_indices[1]].sum(0)
+
+    # TODO calculate a reward from this
 
     return torch.zeros(env.num_envs).to(env.device)
