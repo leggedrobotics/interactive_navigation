809b7cb6a00f986813c626e464546e20c459a5d7
Rafael Cathomen <rafael@cathomen.ch>
2024-09-30 15:49:51 +0200

    bigger terrain, but not too big

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/terrain/terrain.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/terrain/terrain.py
@@ -15,8 +15,8 @@ GAME_ARENA_BASE_CFG = TerrainGeneratorCfg(
     border_width=0.0,
     border_height=0.0,
     curriculum=True,
-    num_rows=1,
-    num_cols=1,
+    num_rows=16,
+    num_cols=16,
     horizontal_scale=0.1,
     vertical_scale=0.1,
     slope_threshold=None,
@@ -30,22 +30,28 @@ GAME_ARENA_BASE_CFG = TerrainGeneratorCfg(
         "wall": CellBorderCfg(
             border_width=0,
             height=2.0,
+            flat_patch_sampling={
+                "init_pos": FlatPatchSamplingCfg(num_patches=25, patch_radius=0.4, max_height_diff=5.0),
+                "lowest_pos": FlatPatchSamplingCfg(
+                    num_patches=25, patch_radius=0.4, max_height_diff=0.5, z_range=(0.0, 0.5)
+                ),
+            },
         ),
     },
 )
 
 GAME_ARENA_RANDOM_FLOORS_CFG = TerrainGeneratorCfg(
-    size=(24.0, 24.0),
+    size=(16.0, 16.0),
     border_width=0.0,
     border_height=0.0,
-    curriculum=True,
-    num_rows=5,  # difficulty levels
-    num_cols=1,  # number of terrains per difficulty level
-    horizontal_scale=0.1,
+    curriculum=False,
+    num_rows=16,  # difficulty levels
+    num_cols=16,  # number of terrains per difficulty level
+    horizontal_scale=0.25,
     vertical_scale=0.1,
     slope_threshold=0.75,
     use_cache=False,
-    difficulty_range=(0.5, 5.4),  # number of steps
+    difficulty_range=(1, 1),  # number of steps
     sub_terrains={
         # "rails": terrain_gen.MeshRailsTerrainCfg(
         #     proportion=0.2,
@@ -58,9 +64,9 @@ GAME_ARENA_RANDOM_FLOORS_CFG = TerrainGeneratorCfg(
             step_height=1,
             min_width=0.5,
             flat_patch_sampling={
-                "init_pos": FlatPatchSamplingCfg(num_patches=25, patch_radius=0.4, max_height_diff=5.0),
+                "init_pos": FlatPatchSamplingCfg(num_patches=128, patch_radius=0.4, max_height_diff=5.0),
                 "lowest_pos": FlatPatchSamplingCfg(
-                    num_patches=25, patch_radius=0.4, max_height_diff=0.5, z_range=(-0.1, 0.5)
+                    num_patches=128, patch_radius=0.4, max_height_diff=0.5, z_range=(0.0, 0.5)
                 ),
             },
         ),
