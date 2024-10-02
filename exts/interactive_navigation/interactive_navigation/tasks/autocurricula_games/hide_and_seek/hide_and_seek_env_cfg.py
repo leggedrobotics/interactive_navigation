bf1efda75ed16b54e500083a2b436fcc50ae4cc7
Rafael Cathomen <rafael@cathomen.ch>
2024-10-02 14:06:21 +0200

    individual actions

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/hide_and_seek_env_cfg.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/hide_and_seek_env_cfg.py
@@ -193,9 +193,19 @@ class ActionsCfg:
     """Action specifications for the MDP."""
 
     # joint_pos = mdp.JointPositionActionCfg(asset_name="robot", joint_names=[".*"], scale=0.5, use_default_offset=True)
-    base_agent = mdp.SimpleActionCfg(
+    # base_agent = mdp.SimpleActionCfg(
+    #     asset_name="robot",
+    #     debug_vis=True,
+    # )
+
+    wrench = mdp.WrenchAction2DCfg(
         asset_name="robot",
         debug_vis=True,
+    )
+
+    jump = mdp.JumpActionCfg(
+        asset_name="robot",
+        debug_vis=False,
     )
 
 
