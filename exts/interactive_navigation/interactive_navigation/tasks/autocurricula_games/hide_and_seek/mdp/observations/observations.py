4c8d20a221e3f28736f7b463ddd24ab130e92daa
Rafael Cathomen <rafael@cathomen.ch>
2024-10-02 17:42:29 +0200

    added box position obs

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/observations/observations.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/observations/observations.py
@@ -36,6 +36,57 @@ def pose_2d_w(env: ManagerBasedEnv, entity_cfg: SceneEntityCfg) -> torch.Tensor:
     return pose_2d
 
 
+def box_pose(env: ManagerBasedEnv, entity_str: str, pov_entity: SceneEntityCfg) -> torch.Tensor:
+    """Returns the pose of all entities relative to the terrain origin.
+    x,y position and heading in the form of cos(theta), sin(theta)."""
+
+    # - box poses
+    box_ids = [asset for asset in list(env.scene.rigid_objects.keys()) if entity_str in asset]
+    box_poses = []
+    box_quats = []
+
+    for box_id in box_ids:
+        box_poses.append(env.scene.rigid_objects[box_id].data.root_pos_w)
+        box_quats.append(env.scene.rigid_objects[box_id].data.root_quat_w)
+
+    boxes_positions_w = torch.stack(box_poses, dim=1)
+    boxes_quats_w = torch.stack(box_quats, dim=1)
+
+    # - robot pose
+    robot = env.scene[pov_entity.name]
+    robot_pos_w = robot.data.root_pos_w
+    robot_quat_w = robot.data.root_quat_w
+
+    # Expand robot pose to match the number of boxes
+    robot_pos_w_expanded = robot_pos_w.unsqueeze(1).expand_as(boxes_positions_w)
+    robot_quat_w_expanded = robot_quat_w.unsqueeze(1).expand_as(boxes_quats_w)
+
+    # - calculate pose of boxes in robot frame
+    t_box_robot, q_box_robot = math_utils.subtract_frame_transforms(
+        robot_pos_w_expanded, robot_quat_w_expanded, boxes_positions_w, boxes_quats_w
+    )
+
+    # Extract x, y positions
+    x = t_box_robot[..., 0]
+    y = t_box_robot[..., 1]
+
+    # Compute yaw angle from the quaternion
+    w = q_box_robot[..., 0]
+    x_q = q_box_robot[..., 1]
+    y_q = q_box_robot[..., 2]
+    z_q = q_box_robot[..., 3]
+
+    # Yaw angle computation
+    sin_yaw = 2 * (w * z_q + x_q * y_q)
+    cos_yaw = 1 - 2 * (y_q * y_q + z_q * z_q)
+
+    # yaw = torch.atan2(sin_yaw, cos_yaw)
+    # Stack the results into a single tensor
+    pose = torch.stack([x, y, cos_yaw, sin_yaw], dim=-1)
+
+    return pose.view(pose.shape[0], -1)
+
+
 def velocity_2d_w(env: ManagerBasedEnv, entity_cfg: SceneEntityCfg) -> torch.Tensor:
     """Returns the velocity of the entity relative to the terrain origin.
     x,y velocity and heading in the form of cos(theta), sin(theta)."""
