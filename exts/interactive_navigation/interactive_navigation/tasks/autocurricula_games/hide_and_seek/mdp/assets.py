94d97736caa5bb522c9b0b6797e4bbf6c8f9983d
Rafael Cathomen <rafael@cathomen.ch>
2024-10-07 16:48:42 +0200

    articulation robot config

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/assets.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/assets.py
@@ -1,11 +1,12 @@
 import omni.isaac.lab.sim as sim_utils
 from omni.isaac.lab.assets import ArticulationCfg, AssetBaseCfg, RigidObject, RigidObjectCfg, AssetBase
+from omni.isaac.lab.actuators import ImplicitActuatorCfg
 
 # - robot:
 ROBOT_CFG = RigidObjectCfg(
     prim_path="{ENV_REGEX_NS}/Robot",
     spawn=sim_utils.CuboidCfg(
-        size=(1.2, 0.8, 0.6),
+        size=(0.6, 0.6, 0.6),
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             max_depenetration_velocity=1.0, disable_gravity=False, max_linear_velocity=10.0, max_angular_velocity=90.0
         ),
@@ -18,6 +19,65 @@ ROBOT_CFG = RigidObjectCfg(
     collision_group=0,
 )
 
+ROBOT_USD_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path="/home/rafael/Projects/MT/interactive_navigation/exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/urdfs/output.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            rigid_body_enabled=True,
+            max_linear_velocity=10.0,
+            max_angular_velocity=10.0,
+            max_depenetration_velocity=100.0,
+            enable_gyroscopic_forces=True,
+        ),
+        mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+            sleep_threshold=0.005,
+            stabilization_threshold=0.001,
+            fix_root_link=True,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 2.0), joint_pos={"joint_x": 0.0, "joint_y": 0.0, "joint_z": 0.0, "joint_yaw": 0.0}
+    ),
+    actuators={
+        "x_actuator": ImplicitActuatorCfg(
+            joint_names_expr=["joint_x"],
+            effort_limit=0.0,
+            velocity_limit=5.0,
+            stiffness=0.0,
+            damping=0.0,
+            friction=0.5,
+        ),
+        "y_actuator": ImplicitActuatorCfg(
+            joint_names_expr=["joint_y"],
+            effort_limit=0.0,
+            velocity_limit=5.0,
+            stiffness=0.0,
+            damping=0.0,
+            friction=0.5,
+        ),
+        "z_actuator": ImplicitActuatorCfg(
+            joint_names_expr=["joint_z"],
+            effort_limit=1000.0,
+            velocity_limit=5.0,
+            stiffness=1000.0,
+            damping=1.0,
+            friction=1.0,
+        ),
+        "yaw_actuator": ImplicitActuatorCfg(
+            joint_names_expr=["joint_yaw"],
+            effort_limit=0.0,
+            velocity_limit=2.0,
+            stiffness=0.0,
+            damping=0.0,
+            friction=0.5,
+        ),
+    },
+)
+
 
 # - movable objects:
 
@@ -25,11 +85,11 @@ ROBOT_CFG = RigidObjectCfg(
 CUBOID_CFG = RigidObjectCfg(
     prim_path="{ENV_REGEX_NS}/Cuboid",
     spawn=sim_utils.CuboidCfg(
-        size=(0.6, 0.5, 0.5),
+        size=(0.75, 0.75, 0.5),
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             max_depenetration_velocity=1.0, disable_gravity=False, max_angular_velocity=3.14, kinematic_enabled=False
         ),
-        mass_props=sim_utils.MassPropertiesCfg(mass=0.5),
+        mass_props=sim_utils.MassPropertiesCfg(mass=100.5),
         physics_material=sim_utils.RigidBodyMaterialCfg(static_friction=0.3, dynamic_friction=0.3),
         collision_props=sim_utils.CollisionPropertiesCfg(),
         visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.9, 0.2, 0.2)),
