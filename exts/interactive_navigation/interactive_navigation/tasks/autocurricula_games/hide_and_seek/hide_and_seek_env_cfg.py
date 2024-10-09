edfa716bb05ca0d5004852fec724f84a71dee4fb
Rafael Cathomen <rafael@cathomen.ch>
2024-10-09 13:00:08 +0200

    change viewer

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/hide_and_seek_env_cfg.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/hide_and_seek_env_cfg.py
@@ -371,20 +371,20 @@ class ViewerCfg:
     """Configuration of the scene viewport camera."""
 
     # eye: tuple[float, float, float] = (-60.0, 0.5, 70.0)
-    eye: tuple[float, float, float] = (0.0, 0.0, 10.0)
+    eye: tuple[float, float, float] = (0.0, 0.0, 30.0)
     """Initial camera position (in m). Default is (7.5, 7.5, 7.5)."""
     # lookat: tuple[float, float, float] = (-60.0, 0.0, -10000.0)
     lookat: tuple[float, float, float] = (0.0, 0.0, 0.0)
     cam_prim_path: str = "/OmniverseKit_Persp"
     resolution: tuple[int, int] = (1280, 720)
-    origin_type: Literal["world", "env", "asset_root"] = "asset_root"
+    origin_type: Literal["world", "env", "asset_root"] = "env"
     """
     * ``"world"``: The origin of the world.
     * ``"env"``: The origin of the environment defined by :attr:`env_index`.
     * ``"asset_root"``: The center of the asset defined by :attr:`asset_name` in environment :attr:`env_index`.
     """
     env_index: int = 0
-    asset_name: str | None = "robot"
+    asset_name: str | None = None  # "robot"
 
 
 ##
