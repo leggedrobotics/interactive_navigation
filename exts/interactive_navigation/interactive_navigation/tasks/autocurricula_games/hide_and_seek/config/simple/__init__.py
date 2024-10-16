285f336a94ea8db7946660777fad435fb5f6aea4
Rafael Cathomen <rafael@cathomen.ch>
2024-10-16 11:26:20 +0200

    added crl gym registration

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/config/simple/__init__.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/config/simple/__init__.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 
 from . import agents, rough_env_cfg
 from interactive_navigation.tasks.autocurricula_games.hide_and_seek.articulation_env_cfg import MoveUpBoxesEnvCfg
+from interactive_navigation.tasks.autocurricula_games.hide_and_seek.articulation_crl_env_cfg import CrlTestEnvCfg
 from interactive_navigation.tasks.autocurricula_games.hide_and_seek.rigid_robot_env_cfg import RigidRobotEnvCfg
 
 ##
@@ -35,6 +36,20 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": RigidRobotEnvCfg,
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:RecurrentPPORunnerCfg",
+    },
+)
+
+##
+# CRL
+##
+
+gym.register(
+    id="Isaac-CRL-TEST-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": CrlTestEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_crl_cfg:TestCrlRunnerCfg",
     },
 )
 
