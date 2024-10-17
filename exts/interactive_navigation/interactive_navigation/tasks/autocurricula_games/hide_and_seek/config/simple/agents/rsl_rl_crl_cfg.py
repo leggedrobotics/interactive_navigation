bfcf7fdda1000873d2a4bdc4d905161d63745e4f
Rafael Cathomen <rafael@cathomen.ch>
2024-10-17 23:46:19 +0200

    new crl configs

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/config/simple/agents/rsl_rl_crl_cfg.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/config/simple/agents/rsl_rl_crl_cfg.py
@@ -13,13 +13,23 @@ from omni.isaac.lab_tasks.utils.wrappers.rsl_rl.crl_cfg import (
 ##
 # Test setup for contrastive RL
 ##
+
+beta_policy_value = 1 / 2
+"""Number of rollouts per step / number of value updates per step"""
+
+beta_actor_value = 1 / 8
+"""number of actor updates per step/ number of value updates per step"""
+
+
 @configclass
 class TestCrlRunnerCfg(RslCRlOnPolicyRunnerCfg):
-    num_steps_per_env = 32
-    buffer_fill_steps = 500
+    num_steps_per_env = 1
+    buffer_fill_steps = 5000
     max_iterations = 100_000
     save_interval = 1000
     experiment_name = "contrastive_RL_test"
+    run_name = "crl_test"
+    wandb_project = "crl_test"
     empirical_normalization = False
     policy = RslRlGoalConditionedActorCfg(
         init_noise_std=1.0,
@@ -32,11 +42,12 @@ class TestCrlRunnerCfg(RslCRlOnPolicyRunnerCfg):
 
     algorithm = RslRlCrlAlgorithmCfg(
         mini_batch_size_and_num_inserts_per_sample=256,
-        stack_N_critic_batches=10,
-        replay_buffer_size=10_000_000,
-        num_critic_learning_steps=13,
-        num_actor_learning_steps=13,
+        stack_N_critic_batches=16,
+        replay_buffer_size=5_000_000,
+        num_critic_learning_steps=2,
+        num_actor_learning_steps=1 / 4,
         entropy_coef=0.001,
+        tau=0.001,
         actor_learning_rate=3e-4,
         critic_learning_rate=3e-4,
         schedule="adaptive",
