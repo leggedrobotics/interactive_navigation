cbaffb3be3b6ba7813ab03640646ee9a9aec248b
Rafael Cathomen <rafael@cathomen.ch>
2024-10-17 23:46:04 +0200

    util import

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/__init__.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/__init__.py
@@ -10,6 +10,7 @@ from .events import *  # noqa: F401, F403
 from .commands import *  # noqa: F401, F403
 
 from . import terrain  # noqa: F401, F403
+from . import utils
 
 from .goals import *  # noqa: F401, F403
 
