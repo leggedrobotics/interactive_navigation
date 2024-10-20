b9c56a212740574034f49775ec78c1b5b2efa3cc
Rafael Cathomen <rafael@cathomen.ch>
2024-10-20 14:12:19 +0200

    docstring

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/config/simple/agents/rsl_rl_crl_cfg.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/config/simple/agents/rsl_rl_crl_cfg.py
@@ -15,17 +15,34 @@ from omni.isaac.lab_tasks.utils.wrappers.rsl_rl.crl_cfg import (
 ##
 
 beta_policy_value = 1 / 2
-"""Number of rollouts per step / number of value updates per step"""
+"""Number of policy updates per step / number of value updates per step"""
 
 beta_actor_value = 1 / 8
-"""number of actor updates per step/ number of value updates per step"""
+"""number of rollout steps per env per step/ number of value updates per step"""
+
+
+# TODO check if this ratio considers the number of envs ie. is it per total transitions or per env transitions
+
+
+"""
+replaybuffer per env = replaybuffer total / num_envs = 5_000_000 / 4096 = 1220
+crit batch size = mini_batch_size_and_num_inserts_per_sample * stack_N_critic_batches = 256 * 2 = 512
+actor batch size = 1024
+
+num_steps_sample_in_buffer = replaybuffer_per_env / num_steps_per_env = 1220 / 5 = 244
+
+expected same sample used for:
+ critic: crit_batch_size * num_critic_learning_steps / num_envs * num_steps_sample_in_buffer = 512 * 8 / 4096 * 244 = 244
+ actor: actor_batch_size * num_actor_learning_steps / num_envs * num_steps_sample_in_buffer = 1024 * 4 / 4096 * 244 = 244
+
+"""
 
 
 @configclass
 class TestCrlRunnerCfg(RslCRlOnPolicyRunnerCfg):
     num_steps_per_env = 1
-    buffer_fill_steps = 5000
-    max_iterations = 100_000
+    buffer_fill_steps = 500
+    max_iterations = 1_000_000
     save_interval = 1000
     experiment_name = "contrastive_RL_test"
     run_name = "crl_test"
@@ -36,21 +53,22 @@ class TestCrlRunnerCfg(RslCRlOnPolicyRunnerCfg):
         activation="elu",
     )
     critic = RslRlContrastiveCriticCfg(
-        representation_dim=128,
+        representation_dim=32,
         activation="elu",
     )
 
     algorithm = RslRlCrlAlgorithmCfg(
         mini_batch_size_and_num_inserts_per_sample=256,
-        stack_N_critic_batches=16,
-        replay_buffer_size=5_000_000,
-        num_critic_learning_steps=2,
-        num_actor_learning_steps=1 / 4,
+        stack_N_critic_batches=2,
+        actor_batch_size=1024,
+        replay_buffer_size=2_000_000,
+        num_critic_learning_steps=16,  # 2,
+        num_actor_learning_steps=4,  # 1 / 4,
         entropy_coef=0.001,
-        tau=0.001,
+        tau=0.005,
         actor_learning_rate=3e-4,
         critic_learning_rate=3e-4,
         schedule="adaptive",
-        gamma=0.99,
+        gamma=0.95,
         max_grad_norm=1.0,
     )
