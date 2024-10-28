fa06a363f4fb052bcc143c9669934937c63b96fb
Rafael Cathomen <rafael@cathomen.ch>
2024-10-28 17:39:54 +0100

    terrain diversity

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/terrain/terrain.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/terrain/terrain.py
@@ -74,25 +74,28 @@ GAME_ARENA_RANDOM_FLOORS_CFG = TerrainGeneratorCfg(
 
 MESH_PYRAMID_TERRAIN_CFG = TerrainGeneratorCfg(
     size=(20.0, 20.0),
-    border_width=1.0,
+    border_width=10.0,
     border_height=10.0,
-    num_rows=16,  # difficulty levels
-    num_cols=16,  # number of terrains per difficulty level
+    num_rows=4,  # difficulty levels
+    num_cols=4,  # number of terrains per difficulty level
     horizontal_scale=0.5,
     vertical_scale=0.05,
     slope_threshold=0.75,
     use_cache=False,
     curriculum=False,
-    difficulty_range=(0, 0.75),
+    difficulty_range=(1.0, 1.0),
     sub_terrains={
         "pyramid_stairs": MeshPyramidTerrainCfg(
-            proportion=1.0,
+            proportion=0.5,
             step_height=1.0,
-            step_width=(5.0, 1.5),
+            step_width=(3.0, 3.0),
             platform_width=3.0,
             border_width=0.0,
             holes=False,
             walls=True,
+            wall_height=10.0,
+            wall_thickness=1.0,
+            type="pyramid",
             flat_patch_sampling={
                 "init_pos": FlatPatchSamplingCfg(
                     num_patches=128,
@@ -109,7 +112,35 @@ MESH_PYRAMID_TERRAIN_CFG = TerrainGeneratorCfg(
                     z_range=(0.75, 50.0),
                 ),
             },
-        )
+        ),
+        "pyramid_stairs_steppable": MeshPyramidTerrainCfg(
+            proportion=0.5,
+            step_height=0.5,
+            step_width=(5.0, 5.0),
+            platform_width=3.0,
+            border_width=0.0,
+            holes=False,
+            walls=True,
+            wall_height=10.0,
+            wall_thickness=1.0,
+            type="pyramid",
+            flat_patch_sampling={
+                "init_pos": FlatPatchSamplingCfg(
+                    num_patches=128,
+                    patch_radius=1.5,
+                    max_height_diff=5.0,
+                ),
+                "lowest_pos": FlatPatchSamplingCfg(
+                    num_patches=128, patch_radius=1.0, max_height_diff=0.5, z_range=(0.0, 0.25)
+                ),
+                "not_lowest_pos": FlatPatchSamplingCfg(
+                    num_patches=128,
+                    patch_radius=0.3,
+                    max_height_diff=50.0,
+                    z_range=(0.75, 50.0),
+                ),
+            },
+        ),
     },
 )
 
