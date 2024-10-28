b8dca530bb0a7f2d0e7ea275805f86a302c183f5
Rafael Cathomen <rafael@cathomen.ch>
2024-10-28 10:40:55 +0100

    more goal commands

--- exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/commands/goal_command.py
+++ exts/interactive_navigation/interactive_navigation/tasks/autocurricula_games/hide_and_seek/mdp/commands/goal_command.py
@@ -59,6 +59,9 @@ class GoalCommand(CommandTerm):
         self.goal_pos_w = torch.zeros((self.num_envs, 3), device=self.device)  # to be sampled
         # goal position in robot frame
         self.goal_pos_b = torch.zeros((self.num_envs, 3), device=self.device)
+        self.goal_pos_env = torch.zeros((self.num_envs, 3), device=self.device)  # in env/terrain frame
+        self.robot_pos_env = torch.zeros((self.num_envs, 3), device=self.device)  # in env/terrain frame
+        # robot pos in robot frame is not necessary, would be identical to 0
 
         # - heading
         # goal heading in world frame. This is THE goal heading angle in the world frame
@@ -68,7 +71,9 @@ class GoalCommand(CommandTerm):
         # heading of the robot (cos(yaw), sin(yaw)).
         self.heading_error = torch.zeros((self.num_envs, 2), device=self.device)
 
-        self.show_line_to_goal = True
+        assert not (
+            self.cfg.only_heading and self.cfg.only_position
+        ), "Cannot have both only_heading and only_position set to True"
 
     def __str__(self) -> str:
         return f"Height Goal Command for {self.cfg.asset_name}"
@@ -79,22 +84,40 @@ class GoalCommand(CommandTerm):
 
     @property
     def command(self) -> torch.Tensor:
-        """The current goal state g(s_t).
+        """We abuse the command to return the goal position and heading error. We use the
+        goal property to condition the policy
+        The current goal state g(s_t).
         Future samples of this are used to train the actor and critic.
         This is also used for observations."""
-        # position (3d) and heading (2d)
+        # Current position (3d) and heading (2d)
         if self.cfg.only_heading:
             return self.heading_error
+        if self.cfg.only_position:
+            if self.cfg.env_frame:
+                # current robot pos in env frame
+                return self.robot_pos_env[..., :2]
+            return self.goal_pos_b[..., :2]
+
         return torch.cat([self.goal_pos_b[..., :2], self.heading_error], dim=1)
 
     @property
     def goal(self) -> torch.Tensor:
         """The desired goal state g(s*).
-        This is used to condition the policy during rollouts."""
-        # this is zero because the desired goal pos in the robot frame is always 0 = at the goal
+        This is used to condition the policy during rollouts.
+        This value is NOT being recorded to the buffer."""
+        # this is zero if in robot frame because the desired goal pos in the robot frame is always 0 = at the goal
         # the goal heading error is 0 --> cos sin of 0 = 1, 0
+
         if self.cfg.only_heading:
             return self.goal_heading_error
+        if self.cfg.only_position:
+            if self.cfg.env_frame:
+                return self.goal_pos_env[..., :2]
+            return torch.zeros_like(self.goal_pos_b[..., :2])
+
+        if self.cfg.env_frame:
+            return torch.cat([self.goal_pos_env[..., :2], self.goal_heading_error], dim=1)
+
         return torch.cat([torch.zeros_like(self.goal_pos_b[..., :2]), self.goal_heading_error], dim=1)
 
     """
@@ -103,8 +126,10 @@ class GoalCommand(CommandTerm):
 
     def _update_metrics(self) -> None:
         """Update the metrics for the goal command."""
-        self.metrics["dist_to_goal"] = torch.linalg.norm(self.goal_pos_b[:, :2], dim=1)
-        self.metrics["heading_error"] = torch.acos(self.heading_error[:, 0])
+        if not self.cfg.only_heading:
+            self.metrics["dist_to_goal"] = torch.linalg.norm(self.goal_pos_b[:, :2], dim=1)
+        if not self.cfg.only_position:
+            self.metrics["heading_error"] = torch.acos(self.heading_error[:, 0])
 
     def _resample_command(self, env_ids: Sequence[int]):
         """The goal is to reach the max height of the current terrain."""
@@ -112,7 +137,19 @@ class GoalCommand(CommandTerm):
         # get potential goals
         terrain = self.env.scene.terrain
 
+        # goal positions
+        random_distance = (
+            torch.rand(len(env_ids), device=self.device)
+            * (self.cfg.goal_radius_range[1] - self.cfg.goal_radius_range[0])
+            + self.cfg.goal_radius_range[0]
+        )
+        random_angle = torch.rand(len(env_ids), device=self.device) * 2 * math.pi
+        random_offset = torch.stack(
+            [torch.cos(random_angle), torch.sin(random_angle)], dim=1
+        ) * random_distance.unsqueeze(1)
+        self.goal_pos_env[env_ids, :2] = random_offset
         self.goal_pos_w[env_ids] = terrain.env_origins[env_ids]
+        self.goal_pos_w[env_ids, :2] += random_offset
 
         # - goal heading
         self.goal_heading[env_ids] = torch.rand(len(env_ids), device=self.device) * 2 * math.pi
@@ -124,7 +161,12 @@ class GoalCommand(CommandTerm):
         robot_pos_w = get_robot_pos(self.robot)
         robot_pos_w[:, 2] -= self.robot_height
         robot_quat = math_utils.yaw_quat(get_robot_quat(self.robot))
+
         self.goal_pos_b, _ = math_utils.subtract_frame_transforms(robot_pos_w, robot_quat, self.goal_pos_w)
+
+        if self.cfg.env_frame:
+            terrain_origins = self.env.scene.terrain.env_origins
+            self.robot_pos_env = robot_pos_w - terrain_origins
 
         # get heading
         yaw_angle = math_utils.euler_xyz_from_quat(robot_quat)[2]
@@ -170,8 +212,12 @@ class GoalCommand(CommandTerm):
             self.box_goal_visualizer.set_visibility(True)
             if self.cfg.show_line_to_goal:
                 self.line_to_goal_visualiser.set_visibility(True)
+            else:
+                self.line_to_goal_visualiser.set_visibility(False)
             if self.cfg.show_goal_heading:
                 self.goal_heading_visualizer.set_visibility(True)
+            else:
+                self.goal_heading_visualizer.set_visibility(False)
         else:
             if hasattr(self, "box_goal_visualizer"):
                 self.box_goal_visualizer.set_visibility(False)
@@ -180,8 +226,6 @@ class GoalCommand(CommandTerm):
 
     def _debug_vis_callback(self, event):
         """Callback function for the debug visualization."""
-        # update the box marker
-        self.box_goal_visualizer.visualize(self.goal_pos_w)
 
         # update the line marker
         # calculate the difference vector between the robot root position and the goal position
@@ -208,6 +252,9 @@ class GoalCommand(CommandTerm):
         line_goal_pos[:, 2] += 1.0
         goal_quat = math_utils.quat_from_angle_axis(self.goal_heading, torch.tensor([0.0, 0.0, 1.0]).to(self.device))
         self.goal_heading_visualizer.visualize(translations=line_goal_pos, orientations=goal_quat)
+
+        # update the box marker
+        self.box_goal_visualizer.visualize(self.goal_pos_w)
 
     ##
     #  Utility functions
